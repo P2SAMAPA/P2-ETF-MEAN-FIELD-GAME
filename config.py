4aaf4dbefd6dc550094b93be2954bf8294f05f68
P2SAMAPA <pranabpattanaik@gmail.com>
2026-07-14 08:21:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ UNIVERSES = {
     "COMBINED": [
         "TLT", "VCIT", "LQD", "HYG", "VNQ", "GLD", "SLV",
         "SPY", "QQQ", "XLK", "XLF", "XLE", "XLV", "XLI", "XLY",
-        "XLP", "XLU", "GDX", "XME", "IWF", "XSD", "XBI",
+        "XLP", "XLU", "GDX", "XME", "IWF", "XSD", "XBI", "SMH", "SOXX", "XLB",
         "IWM", "IWD", "IWO", "XLB", "XLRE",
     ],
 }
